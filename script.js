3c7f6d75abd90d7d8935a619cfbd76579e8f1b46
v-sec0 <122383941+v-sec0@users.noreply.github.com>
2023-09-06 11:36:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    console.log("Ready to go!");
+    var clickCount = 0;
+  
+    // Introduction
+    $("#btnMain").click(function() {
+        clickCount++;
+        switch (clickCount) {
+            case 1:
+                $("#welcomeMessageOne").fadeIn();
+                theButton.text("Continue");
+                break;   
+            case 2:
+                $("#welcomeMessageTwo").fadeIn();
+                break;
+            case 3:
+                $("#concernedCitizen").fadeIn();
+                break;
+            case 4:
+                $("#concernedDialouge").fadeIn();
+                break;
+            case 5:
+                $("#welcomeMessageThree").fadeIn();
+                theButton.text("Enter the City");
+                break;
+            case 6:
+                $("#wholeIntro").hide();
+                theScene.fadeIn();
+                theButton.text("Continue");
+                break;
+            case 7:
+                $("#sceneOne").fadeIn();
+                break;
+            case 8:
+                $("#sceneTwo").fadeIn();
+                break;
+            case 9:
+                $("#sceneThree").fadeIn();
+                $("#scenePrompt").fadeIn();
+                break;
+            case 10:
+                theGuard.fadeIn();
+                theMerchant.fadeIn();
+                thePriest.fadeIn();
+                theButton.text("Reset");
+                theButton.hide();
+                break;  
+            default:
+                location.reload();  
+            
+
+        }
+    });
+
+
+    // Variables Assigned
+    let theGuard = $("#theGuard");
+    let thePriest = $("#thePriest");
+    let theMerchant = $("#theMerchant");
+    let theScene = $("#wholeScene");
+    let theButton = $("#btnMain")
+
+
+    // The Merchant ending
+    theMerchant.click(function() {
+        theScene.hide();
+        theGuard.hide();
+        thePriest.hide();
+        $("#theMerchantEnding").fadeIn();
+        theButton.fadeIn();
+        
+    });
+
+    // The Guard ending
+    theGuard.click(function() {
+        theScene.hide();
+        theMerchant.hide();
+        thePriest.hide();
+        $("#theGuardEnding").fadeIn();
+        theButton.fadeIn();
+    });
+
+    // The Priest ending
+    thePriest.click(function() {
+       theScene.hide();
+        theMerchant.hide();
+        theGuard.hide();
+        $("#thePriestEnding").fadeIn();
+        theButton.fadeIn();
+    });
+
+
+});
